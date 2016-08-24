08abac19e51a7075606cc3bbe79fc1a1b9aaaca3
Duncan Adamson <2073411a@student.gla.ac.uk>
2016-08-24 16:19:53 +0100

    Added the ability to only grab part of the screen

--- stream.py
+++ stream.py
@@ -1,7 +1,14 @@
 import pyscreenshot
 import time
+import sys
 
-while True:
-  i = pyscreenshot.grab()
-  i.save("screenshot.png")
-  time.sleep(0.1)
+if len(sys.argv) < 5:
+  while True:
+    i = pyscreenshot.grab()
+    i.save("screenshot.png")
+    time.sleep(0.1)
+else:
+  while True:
+    i = pyscreenshot.grab(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+    i.save("screenshot.png")
+    time.sleep(0.1)
